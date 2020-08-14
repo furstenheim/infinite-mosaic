99ce7fac424b7c3358315f233146be37367984cc
Gabriel <furstenheim@gmail.com>
2020-08-14 16:40:38 +0200

    Compute frame of rectangle

---
+++ process/go.mod
@@ -0,0 +1,5 @@
+module process
+
+go 1.14
+
+require github.com/stretchr/testify v1.6.1
--- process/main.go
+++ process/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"image"
+	_ "image/jpeg"
 	"log"
 	"os"
 	"path/filepath"
@@ -40,4 +42,37 @@ func main () {
 
 func processJPG (path string, info os.FileInfo) {
 	log.Println(path)
+	imgfile, openErr := os.Open(path)
+	if openErr != nil {
+		log.Fatal(openErr)
+	}
+	defer imgfile.Close()
+	img, _, decodeErr := image.Decode(imgfile)
+	if decodeErr != nil {
+		log.Fatal(decodeErr, " file ", path)
+	}
+	log.Println(img.Bounds().Dy())
+}
+
+func fromRect (rectangle image.Rectangle) Frame {
+	size := minInt(rectangle.Dx(), rectangle.Dy())
+	minX := rectangle.Min.X + (rectangle.Dx() - size) / 2
+	maxX := rectangle.Max.X - (rectangle.Dx() - size) / 2
+	minY := rectangle.Min.Y + (rectangle.Dy() - size) / 2
+	maxY := rectangle.Max.Y - (rectangle.Dy() - size) / 2
+	return Frame{MinX: minX, MinY: minY, MaxX: maxX, MaxY: maxY}
+}
+
+func minInt (a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+type Frame struct {
+	MinX int
+	MinY int
+	MaxX int
+	MaxY int
 }
