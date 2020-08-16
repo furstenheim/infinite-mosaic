e79677ac6fa9523d6c2cf685d460d871c9ea38b4
Gabriel <furstenheim@gmail.com>
2020-08-16 10:30:39 +0200

    Display leaflet map

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
 import './main.scss'
 import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+import processed from './process/output.json'
+import 'regenerator-runtime/runtime'
+
 const MOSAIC_ID = 'mosaic-holder'
 const LOADING_CONTENT = 'loading-content'
-console.log('hello world 2')
+const MAP_SIZE = 1000
+const TILE_SIZE = 10
+const CANVAS_SIZE = MAP_SIZE / TILE_SIZE
+const DOWNLOADED_IMAGE_SIZE = 400
 main()
-function main () {
+
+/**
+ * @typedef {
+ * {avg: {R: number, G: number, B: number, A: number},
+ * frame: {"minX":number,"minY":number,"maxX":number,"maxY":number},
+ * name: string}} ProcessedImage
+ */
+async function main () {
+  /**
+   *
+   * @type ProcessedImage
+   */
+
+  const img = processed[0]
+  const placeholderCanvas = document.createElement('canvas')
+  const context = placeholderCanvas.getContext('2d')
+
+  placeholderCanvas.height = CANVAS_SIZE
+  placeholderCanvas.width = CANVAS_SIZE
+  await new Promise(function (resolve, reject) {
+    const baseImage = new window.Image()
+    baseImage.src = img.name
+    baseImage.onload = function () {
+      context.drawImage(baseImage, img.frame.minX, img.frame.minY, img.frame.maxX - img.frame.minX, img.frame.maxY - img.frame.minY, 0, 0, CANVAS_SIZE, CANVAS_SIZE)
+      resolve()
+    }
+  })
+
+ //  document.body.appendChild(placeholderCanvas)
   document.getElementById(LOADING_CONTENT).remove()
   const map = L.map(MOSAIC_ID, {
     minZoom: -2,
     maxZoom: 20
   })
-  map.setView([0, 0], 11)
+  map.setView([0, 0], 4)
+  let i = 0
   const layer = new (L.GridLayer.extend({
-    createTile: function (coords, done) {
-      const tile = L.DomUtil.create('canvas', 'leaflet-tile')
+    createTile: function (coords) {
+      const tile = L.DomUtil.create('canvas', `leaflet-tile tile-${coords.x}-${coords.y}`)
       const size = this.getTileSize()
+      console.log(i++, coords.x, coords.y)
       tile.width = size.x
       tile.height = size.y
       const ctx = tile.getContext('2d')
@@ -23,16 +61,17 @@ function main () {
       return tile
     }
   }))({
-    tileSize: 4 **2
+    tileSize: TILE_SIZE,
+    zoom: 0
   })
   layer.addTo(map)
 }
 
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
+function getRandomColor () {
+  var letters = '0123456789ABCDEF'
+  var color = '#'
   for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)]
   }
-  return color;
+  return color
 }
