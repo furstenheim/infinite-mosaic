1843fadaadebb6c31d0512f3a8bc3a26dddccea1
Gabriel <furstenheim@gmail.com>
2020-08-16 22:31:33 +0200

    Compute tiling

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ import './main.scss'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
+/**
+ * @type Array<ProcessedImage>
+ */
 import processed from './process/output.json'
 import 'regenerator-runtime/runtime'
 
 const MOSAIC_ID = 'mosaic-holder'
 const LOADING_CONTENT = 'loading-content'
 const MAP_SIZE = 1000
-const TILE_SIZE = 100
+// const TILE_SIZE = 10
+const TILE_SIZE = 10
 const CANVAS_SIZE = MAP_SIZE / TILE_SIZE
 const DOWNLOADED_IMAGE_SIZE = 400
 main()
@@ -20,12 +24,8 @@ main()
  * name: string}} ProcessedImage
  */
 async function main () {
-  /**
-   *
-   * @type ProcessedImage
-   */
-
-  const img = processed[0]
+  // const img = processed[0]
+  const img = {"avg":{"R":90,"G":85,"B":70,"A":0},"frame":{"minX":0,"minY":40,"maxX":400,"maxY":440},"name":"https://www.artic.edu/iiif/2/0c8fb799-31bb-fb44-b863-080a614966f1/full/400,/0/default.jpg"}
   const placeholderCanvas = document.createElement('canvas')
   const context = placeholderCanvas.getContext('2d')
 
@@ -33,7 +33,9 @@ async function main () {
   placeholderCanvas.width = CANVAS_SIZE
   await new Promise(function (resolve, reject) {
     const baseImage = new window.Image()
-    baseImage.src = img.name
+    // baseImage.src = `scrape/downloaded/${img.name.replace(/\//g, '_')}`
+    baseImage.src = 'a.jpg'
+    baseImage.crossOrigin = 'Anonymous'
     baseImage.onload = function () {
       context.drawImage(baseImage, img.frame.minX, img.frame.minY, img.frame.maxX - img.frame.minX, img.frame.maxY - img.frame.minY, 0, 0, CANVAS_SIZE, CANVAS_SIZE)
       resolve()
@@ -49,24 +51,42 @@ async function main () {
     crs: L.CRS.Simple
   })
   map.setView([0, 0], 4)
-  let i = 0
-  const layer = new (L.GridLayer.extend({
+  const i = 0
+  const imageData = context.getImageData(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+  const xMin = map.getCenter().lng - CANVAS_SIZE / 2
+  const yMin = map.getCenter().lat - CANVAS_SIZE / 2
+  const BaseContextLayer = L.GridLayer.extend({
     createTile: function (coords) {
-      const tile = L.DomUtil.create('canvas', `leaflet-tile tile-${coords.x}-${coords.y}`)
+      // return debugCoords(coords)
+      const tile = L.DomUtil.create('img', `leaflet-tile tile-${coords.x}-${coords.y}`)
+      const baseCoord = (coords.x - xMin + (coords.y - yMin) * CANVAS_SIZE) * 4
+      const r = imageData.data[baseCoord]
+      const g = imageData.data[baseCoord + 1]
+      const b = imageData.data[baseCoord + 2]
+      const minImage = findMin(r, g, b)
+      tile.src = minImage.name.replace('full/400', `full/${TILE_SIZE}`)
+/*
       const size = this.getTileSize()
-      console.log(i++, coords.x, coords.y)
+      // console.log(i++, coords.x, coords.y)
       tile.width = size.x
       tile.height = size.y
       const ctx = tile.getContext('2d')
       ctx.fillStyle = getRandomColor()
       ctx.fillRect(0, 0, size.x, size.y)
+*/
       return tile
     }
-  }))({
+  })
+  const BaseUrlLayer = L.TileLayer.extend({
+    getTileUrl: function (coords) {
+    }
+  })
+  const layer = new (BaseContextLayer)({
     tileSize: TILE_SIZE,
     zoom: 0
   })
   layer.addTo(map)
+  window.layer = layer
   window.map = map
 }
 
@@ -77,4 +97,31 @@ function getRandomColor () {
     color += letters[Math.floor(Math.random() * 16)]
   }
   return color
+}
+
+/**
+ *
+ * @param r
+ * @param g
+ * @param b
+ * @returns ProcessedImage
+ */
+function findMin (r, g, b) {
+  let minDistance = Infinity
+  let minPicture
+  for (const processedImage of processed) {
+    const distance = (processedImage.avg.R - r) ** 2 + (processedImage.avg.G - g) ** 2 + (processedImage.avg.B - b) ** 2
+    if (distance < minDistance) {
+      minPicture = processedImage
+      minDistance = distance
+    }
+  }
+  return minPicture
+}
+
+function debugCoords (coords) {
+  var tile = document.createElement('div')
+  tile.innerHTML = [coords.x, coords.y, coords.z].join(', ')
+  tile.style.outline = '1px solid red'
+  return tile
 }
