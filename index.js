bcc983eb2aecf36f7dbe430ace480730e5378605
Gabriel <furstenheim@gmail.com>
2020-08-21 14:55:14 +0200

    Load images from sprite

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 import './main.scss'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
-
+import _ from 'lodash'
 /**
- * @type Array<ProcessedImage>
+ * @type {{ExportedImages: Array<ProcessedImage>}}
  */
 import processed from './process/output.json'
 import 'regenerator-runtime/runtime'
-
 const MOSAIC_ID = 'mosaic-holder'
 const LOADING_CONTENT = 'loading-content'
 const MAP_SIZE = 1000
 // const TILE_SIZE = 10
-const TILE_SIZE = 20
+const TILE_SIZE = 10
 const CANVAS_SIZE = MAP_SIZE / TILE_SIZE
 const DOWNLOADED_IMAGE_SIZE = 400
 main()
-
+let sprites = []
+const SIDE = processed.Side
+const tile2Sprite = {
+  10: {index: 0, size: 10},
+  20: {index: 1, size: 20}
+}
 /**
  * @typedef {
  * {avg: {R: number, G: number, B: number, A: number},
  * frame: {"minX":number,"minY":number,"maxX":number,"maxY":number},
- * name: string}} ProcessedImage
+ * name: string, id: string}} ProcessedImage
  */
 async function main () {
-  // const img = processed[0]
-  const img = {"avg":{"R":90,"G":85,"B":70,"A":0},"frame":{"minX":0,"minY":40,"maxX":400,"maxY":440},"name":"https://www.artic.edu/iiif/2/0c8fb799-31bb-fb44-b863-080a614966f1/full/400,/0/default.jpg"}
+  // const img = processed.ExportedImages[0]
+  const img = {"avg":{"R":90,"G":85,"B":70,"A":0},"frame":{"minX":0,"minY":40,"maxX":400,"maxY":440},"name":"https://www.artic.edu/iiif/2/0c8fb799-31bb-fb44-b863-080a614966f1/full/400,/0/default.jpg","id":"0d391d13-74b0-0cf2-85f0-51dc499bed81"}
   const placeholderCanvas = document.createElement('canvas')
   const context = placeholderCanvas.getContext('2d')
 
@@ -34,13 +38,22 @@ async function main () {
   await new Promise(function (resolve, reject) {
     const baseImage = new window.Image()
     // baseImage.src = `scrape/downloaded/${img.name.replace(/\//g, '_')}`
-    baseImage.src = 'a.jpg'
+    baseImage.src = `squared-images/${img.id}.jpeg`
     baseImage.crossOrigin = 'Anonymous'
     baseImage.onload = function () {
-      context.drawImage(baseImage, img.frame.minX, img.frame.minY, img.frame.maxX - img.frame.minX, img.frame.maxY - img.frame.minY, 0, 0, CANVAS_SIZE, CANVAS_SIZE)
+      context.drawImage(baseImage, 0, 0, CANVAS_SIZE, CANVAS_SIZE)
       resolve()
     }
   })
+  sprites = await Promise.all(_.times(3, function (i) {
+    return new Promise(function (resolve) {
+      const sprite = new window.Image()
+      sprite.src = `squared-images/sprite${i}.jpeg`
+      sprite.onload = function () {
+        resolve(sprite)
+      }
+    })
+  }))
 
   document.body.appendChild(placeholderCanvas)
   document.getElementById(LOADING_CONTENT).remove()
@@ -56,7 +69,7 @@ async function main () {
   const xMin = map.getCenter().lng - CANVAS_SIZE / 2
   const yMin = map.getCenter().lat - CANVAS_SIZE / 2
   const BaseContextLayer = L.GridLayer.extend({
-    createTile: function (coords, done) {
+    createTile: function (coords) {
       // return debugCoords(coords)
       const tile = L.DomUtil.create('canvas', `leaflet-tile tile-${coords.x}-${coords.y}`)
       tile.height = TILE_SIZE
@@ -65,26 +78,40 @@ async function main () {
       const r = imageData.data[baseCoord]
       const g = imageData.data[baseCoord + 1]
       const b = imageData.data[baseCoord + 2]
-      const minImage = findMin(r, g, b)
-      const tileImage = new window.Image()
-      // tileImage.src = `scrape/downloaded/${img.name.replace(/\//g, '_')}`
-      if (minImage.frame.minX === 0) {
-        // img is vertical
-        tileImage.src = minImage.name.replace('full/400', `full/${TILE_SIZE}`)
-      } else {
-        tileImage.src = minImage.name.replace('full/400', `full/${parseInt(400 * TILE_SIZE / (minImage.frame.maxX - minImage.frame.minX))}`)
-      }
+      const { image: minImage, index: imageIndex} = findMin(r, g, b)
+      const spriteConfig = tile2Sprite[TILE_SIZE]
 
-      tileImage.onload = function () {
-        // debugger
+      if (!spriteConfig) {
+        const tileImage = new window.Image()
+        // tileImage.src = `scrape/downloaded/${img.name.replace(/\//g, '_')}`
+        if (minImage.frame.minX === 0) {
+          // img is vertical
+          tileImage.src = minImage.name.replace('full/400', `full/${TILE_SIZE}`)
+        } else {
+          tileImage.src = minImage.name.replace('full/400', `full/${parseInt(400 * TILE_SIZE / (minImage.frame.maxX - minImage.frame.minX))}`)
+        }
+
+        tileImage.onload = function () {
+          // debugger
+          const context = tile.getContext('2d')
+          context.height = tile.height
+          context.width = tile.width
+          context.drawImage(tileImage, parseInt(minImage.frame.minX * TILE_SIZE / 400), parseInt(minImage.frame.minY * TILE_SIZE / 400), TILE_SIZE, TILE_SIZE, 0, 0, TILE_SIZE, TILE_SIZE)
+
+          // context.drawImage(tileImage, parseInt(minImage.frame.minX * TILE_SIZE / 400), parseInt(minImage.frame.minY * TILE_SIZE / 400), parseInt((minImage.frame.maxX - minImage.frame.minX) * TILE_SIZE / 400), parseInt((minImage.frame.maxY - minImage.frame.minY)* TILE_SIZE / 400), 0, 0, tile.width, tile.height)
+          done()
+        }
+      } else {
         const context = tile.getContext('2d')
         context.height = tile.height
         context.width = tile.width
-        context.drawImage(tileImage, parseInt(minImage.frame.minX * TILE_SIZE / 400), parseInt(minImage.frame.minY * TILE_SIZE / 400), TILE_SIZE, TILE_SIZE, 0, 0, TILE_SIZE, TILE_SIZE)
-
-        // context.drawImage(tileImage, parseInt(minImage.frame.minX * TILE_SIZE / 400), parseInt(minImage.frame.minY * TILE_SIZE / 400), parseInt((minImage.frame.maxX - minImage.frame.minX) * TILE_SIZE / 400), parseInt((minImage.frame.maxY - minImage.frame.minY)* TILE_SIZE / 400), 0, 0, tile.width, tile.height)
-        done()
+        const sprite = sprites[spriteConfig.index]
+        context.drawImage(sprite, (imageIndex % SIDE) * spriteConfig.size, parseInt(imageIndex / SIDE) * spriteConfig.size, spriteConfig.size, spriteConfig.size, 0, 0, TILE_SIZE, TILE_SIZE)
+        // context.drawImage(sprite, 1 * spriteConfig.size, 1 * spriteConfig.size, spriteConfig.size, spriteConfig.size, 0, 0, TILE_SIZE, TILE_SIZE)
+        // setTimeout(done)
       }
+
+
       // TODO set offset
       // tile.src =
 /*
@@ -126,19 +153,23 @@ function getRandomColor () {
  * @param r
  * @param g
  * @param b
- * @returns ProcessedImage
+ * @returns {{index: number, image: ProcessedImage}}
  */
 function findMin (r, g, b) {
   let minDistance = Infinity
   let minPicture
-  for (const processedImage of processed) {
+  let processedImage
+  let minIndex = -1
+  for (let i = 0; i < processed.ExportedImages.length; i++) {
+    processedImage = processed.ExportedImages[i]
     const distance = (processedImage.avg.R - r) ** 2 + (processedImage.avg.G - g) ** 2 + (processedImage.avg.B - b) ** 2
     if (distance < minDistance) {
       minPicture = processedImage
       minDistance = distance
+      minIndex = i
     }
   }
-  return minPicture
+  return { index: minIndex, image: minPicture }
 }
 
 function debugCoords (coords) {
