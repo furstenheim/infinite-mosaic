4434bfee50f0f4d12a9b91938d80ab436441fffd
Gabriel <furstenheim@gmail.com>
2020-10-23 22:41:23 +0200

    Wait for all images to be rendered

--- index.js
+++ index.js
@@ -124,6 +124,11 @@ async function main () {
     // const deltaY = deltaYBase - deltaYBase % 4
     // let canvasIndex = 0
     // const context = canvas.getContext('2d')
+    let remainingImages = 0
+    let renderingDone
+    const finishedRenderingPromise = new Promise(function (resolve) {
+      renderingDone = resolve
+    })
     for (let j = 0; j < height / tileSize; j++) {
       for (let i = 0; i < width / tileSize; i++ && canvasIndex++) {
         const currentXInGrid = xInGrid + i
@@ -148,100 +153,30 @@ async function main () {
           const tileImage = new window.Image()
           // baseImage.src = `scrape/downloaded/${img.name.replace(/\//g, '_')}`
           tileImage.src = `squared-images/${minImage.id}.jpeg`
+          remainingImages++
           tileImage.onload = function () {
+            remainingImages--
+
             // if (thisExecution === currentExecution) {
-              context.drawImage(tileImage, 0, 0, tileImage.width, tileImage.height, i * tileSize, j * tileSize, tileSize, tileSize)
-              // Prevent drawing after double zoom
+            context.drawImage(tileImage, 0, 0, tileImage.width, tileImage.height, i * tileSize, j * tileSize, tileSize, tileSize)
+
+            // Prevent drawing after double zoom
             // }
+            if (remainingImages === 0) {
+              renderingDone()
+            }
           }
         }
       }
       canvasIndex += deltaY + 1
     }
+    if (remainingImages !== 0) {
+      await finishedRenderingPromise
+    }
     displayedContext.drawImage(context.canvas, parseInt((floatXCoordinateInGrid - xInGrid) * tileSize), parseInt((floatYCoordinateInGrid - yInGrid) * tileSize), width / scaleFactor, height / scaleFactor, 0, 0, width, height)
   }
 }
 
-function leafletMosaic () {
-  const map = L.map(MOSAIC_ID, {
-    minZoom: 0,
-    maxZoom: 20,
-    zoomControl: false,
-    crs: L.CRS.Simple,
-    maxBounds: new L.LatLngBounds(new L.LatLng(MAP_SIZE / 2, -MAP_SIZE / 2), new L.LatLng(-MAP_SIZE / 2, MAP_SIZE / 2))
-  })
-  map.setView([0, 0], 0)
-
-  const layer = new (L.CanvasLayer.extend({
-    /**
-     *
-     * @param {{bounds: LatLngBounds, canvas: canvas, center: {x: number, y: number},
-     * corner: {x: number, y: number}
-     *   layer: Layer,
-     *   size: Point,
-     *   zoom: number}} canvasOverlay
-     */
-    async onDrawLayer (canvasOverlay) {
-      const drawZoom = canvasOverlay.zoom
-      currentExecution++
-      const thisExecution = currentExecution
-      const depth = (drawZoom - drawZoom % tile2Sprite.length) / tile2Sprite.length
-      if (!cachedBestImages[depth]) {
-        cachedBestImages[depth] = new Uint16Array(getSide(depth) * getSide(depth))
-      }
-      const spriteConfig = tile2Sprite[drawZoom % (tile2Sprite.length)]
-      const tileSize = spriteConfig.size
-      const width = canvasOverlay.size.x
-      const height = canvasOverlay.size.y
-      const northWest = canvasOverlay.bounds.getNorthWest()
-      // const baseCoord = (coords.x - xMin + (coords.y - yMin) * CANVAS_SIZE) * 4
-      const xInGrid = parseInt((northWest.lng / MIN_TILE_SIZE + CANVAS_SIZE / 2) * getSide(depth - 1))
-      const yInGrid = parseInt((CANVAS_SIZE / 2 - northWest.lat / MIN_TILE_SIZE) * getSide(depth - 1))
-      if (xInGrid < 0 || yInGrid < 0) {
-        // We'll be called again
-        return
-      }
-      let canvasIndex = xInGrid + yInGrid * getSide(depth)
-      // canvasIndex -= canvasIndex % 4
-      const deltaY = getSide(depth) - parseInt(CANVAS_SIZE / tileSize * MIN_TILE_SIZE) // canvas size - width
-
-      // const deltaY = deltaYBase - deltaYBase % 4
-      // let canvasIndex = 0
-      const context = canvasOverlay.canvas.getContext('2d')
-      for (let j = 0; j < height / tileSize; j++) {
-        for (let i = 0; i < width / tileSize; i++) {
-          let imageIndex
-          if (cachedBestImages[depth][canvasIndex]) {
-            imageIndex = cachedBestImages[depth][canvasIndex]
-          } else {
-            imageIndex = await computeAndMemoize(xInGrid + i, yInGrid + j, depth)
-          }
-          const adaptedIndex = imageIndex - 1
-          canvasIndex++
-          const minImage = processed.ExportedImages[imageIndex]
-          if (spriteConfig.type === SPRITE) {
-            const sprite = sprites[spriteConfig.index]
-            context.drawImage(sprite, (adaptedIndex % SIDE) * spriteConfig.size, parseInt(adaptedIndex / SIDE) * spriteConfig.size, spriteConfig.size, spriteConfig.size, i * tileSize, j * tileSize, tileSize, tileSize)
-          } else {
-            const tileImage = new window.Image()
-            // baseImage.src = `scrape/downloaded/${img.name.replace(/\//g, '_')}`
-            tileImage.src = `squared-images/${minImage.id}.jpeg`
-            tileImage.onload = function () {
-              if (thisExecution === currentExecution) {
-                context.drawImage(tileImage, 0, 0, tileImage.width, tileImage.height, i * tileSize, j * tileSize, tileSize, tileSize)
-                // Prevent drawing after double zoom
-              }
-            }
-          }
-        }
-        canvasIndex += deltaY
-      }
-    }
-  }))()
-  layer.addTo(map)
-  window.layer = layer
-  window.map = map
-}
 function getRandomNumber (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
